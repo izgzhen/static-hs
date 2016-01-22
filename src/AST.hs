7362f971ca5d14de7f8705536d07b9cd2e3111d8
Zhen Zhang <izgzhen@gmail.com>
2016-01-23 00:27:47 +0800

    even more abstraction shit

--- src/AST.hs
+++ src/AST.hs
@@ -59,22 +59,11 @@ instance Show ROp where
 showInfix :: (Show a, Show b, Show c) => a -> b -> c -> String
 showInfix a1 op a2 = "(" ++ show a1 ++ " " ++ show op ++ " " ++ show a2 ++ ")"
 
--- Free variable
-class FV a where
-    fv :: a -> Set Name
-
-instance FV AExp where
-    fv (AVar x) = singleton x
-    fv (ANum _) = empty
-    fv (AInfix e1 _ e2) = fv e1 `union` fv e2
-
-
--- Get sub arith expressions of statement
--- Used by AEA
-
 nonTrivial :: AExp -> Bool
 nonTrivial (AInfix _ _ _) = True
 nonTrivial _ = False
+
+-- Some abstract shit
 
 class Recursive a e where
     recursive :: Container m e => a -> m e
@@ -82,25 +71,26 @@ class Recursive a e where
 class Monoid (m e) => Container m e where
     single :: e -> m e
 
+class Collect a where
+    collect :: Monoid m => (a -> m) -> a -> m
 
 instance Recursive AExp a => Recursive BExp a where
-    recursive (BLit _)          = mempty
-    recursive (BNot bexp)       = recursive bexp
-    recursive (BInfixB b1 _ b2) = recursive b1 `mappend` recursive b2
-    recursive (BInfixA a1 _ a2) = recursive a1 `mappend` recursive a2
+    recursive = collect f
+        where
+            f (BInfixA a1 _ a2) = recursive a1 `mappend` recursive a2
+            f _ = mempty
 
-instance Recursive AExp AExp where
-    recursive a@(AVar _)         = single a
-    recursive a@(ANum _)         = single a
-    recursive a@(AInfix a1 _ a2) = mconcat [single a, recursive a1, recursive a2]
+instance Collect a => Recursive a a where
+    recursive = collect single
 
 instance Recursive AExp Name where
-    recursive (AVar x)         = single x
-    recursive (ANum _)         = mempty
-    recursive (AInfix a1 _ a2) = mconcat [recursive a1, recursive a2]
+    recursive = collect f
+        where
+            f (AVar x) = single x
+            f _        = mempty
 
 instance Recursive (Stmt a) AExp where
-    recursive = collectStmt f
+    recursive = collect f
         where
             f :: Container m AExp => Stmt a -> m AExp
             f (Assign _ _ aexp)          = recursive aexp
@@ -109,7 +99,7 @@ instance Recursive (Stmt a) AExp where
             f _                          = mempty
 
 instance Recursive (Stmt a) Name where
-    recursive = collectStmt f
+    recursive = collect f
         where
             f :: Container m Name => Stmt a -> m Name
             f (Assign _ x aexp)          = single x `mappend` recursive aexp
@@ -117,15 +107,32 @@ instance Recursive (Stmt a) Name where
             f (While (bexp, _) _)        = recursive bexp
             f _                          = mempty
 
-collectStmt :: Monoid m => (Stmt a -> m) -> Stmt a -> m
-collectStmt f s = case s of
-    (Assign _ _ _)       -> f s
-    (Skip _)             -> mempty
-    (Seq s1 s2)          -> collectStmt' s1 `mappend` collectStmt' s2
-    (IfThenElse _ s1 s2) -> mconcat [f s, collectStmt' s1, collectStmt' s2]
-    (While _ s)          -> f s `mappend` collectStmt' s
-    where
-        collectStmt' = collectStmt f
+instance Collect (Stmt a) where
+    collect f s = f s `mappend` case s of
+        Assign _ _ _       -> mempty
+        Skip _             -> mempty
+        Seq s1 s2          -> collect' s1 `mappend` collect' s2
+        IfThenElse _ s1 s2 -> collect' s1 `mappend` collect' s2
+        While _ s          -> collect' s
+        where
+            collect' = collect f
+
+instance Collect BExp where
+    collect f a = f a `mappend` case a of
+        BLit _          -> mempty
+        BNot bexp       -> collect' bexp
+        BInfixB b1 _ b2 -> collect' b1 `mappend` collect' b2
+        BInfixA a1 _ a2 -> mempty
+        where
+            collect' = collect f
+
+instance Collect AExp where
+    collect f a = f a `mappend` case a of
+        AVar _         -> mempty
+        ANum _         -> mempty
+        AInfix a1 _ a2 -> collect' a1 `mappend` collect' a2
+            where
+                collect' = collect f
 
 instance Ord a => Container Set a where
     single = singleton
